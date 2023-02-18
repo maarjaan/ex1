dc449a1cfb9598c6e0108e27f0bded4603bff73f
maarjaan <124882750+maarjaan@users.noreply.github.com>
2023-02-18 18:35:00 +0330

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,12 @@
+num5 ="10"
+num6 = "3"
+num7 = (float(num5))
+num8= (int(num6))
+div4 = num7/num8
+print(div4)
+x = round(div4,2)
+print(x)
+div5 = num7//num8
+print(div5)
+reminder = num7%num8
+print(reminder)
